97728d7337f5d2253283d9080bc2b8f16d930791
Jesper Axelsson <jesperaxe@gmail.com>
2024-07-21 12:00:46 +0200

    Refactor input

--- src/app_data.rs
+++ src/app_data.rs
@@ -4,7 +4,7 @@ use std::io::{Seek, SeekFrom};
 use std::{fs::metadata, io, time::Duration};
 use std::{fs::File, io::Read};
 
-use crossterm::event;
+use crossterm::event::{self, KeyEvent};
 use crossterm::event::{Event, KeyCode, KeyEventKind};
 
 use tui_textarea::TextArea;
@@ -165,107 +165,131 @@ impl<'a> App<'a> {
                 }
 
                 match self.app_mode {
-                    AppMode::Normal => {
-                        match key.code {
-                            KeyCode::Char('f') => self.follow_mode = !self.follow_mode,
-                            KeyCode::Char('c') => {
-                                self.list_items.clear_all();
-                                self.log_textarea = None;
-                            }
-                            KeyCode::Char('x') => self.list_items.set_cutoff(0),
-                            KeyCode::Char('?') => self.app_mode = AppMode::ShowingKeybindings,
-                            KeyCode::Char('/') => self.app_mode = AppMode::EditingFilter,
-                            KeyCode::Tab => self.app_mode = AppMode::FocusLogText,
+                    AppMode::Normal => self.handle_events_log_list(key)?,
+                    AppMode::FocusLogText => self.handle_events_log_text(key)?,
+                    AppMode::EditingFilter => self.handle_events_filter(key)?,
+                    AppMode::ShowingKeybindings => self.handle_events_show_keybindings(key)?,
+                }
+            }
+        }
 
-                            KeyCode::PageUp => self
-                                .list_items
-                                .jump_relative(-((self.size.height - 4) as isize)),
-                            KeyCode::PageDown => self
-                                .list_items
-                                .jump_relative((self.size.height - 4) as isize),
+        Ok(())
+    }
 
-                            KeyCode::Home => self.list_items.goto_start(),
-                            KeyCode::End => self.list_items.goto_end(),
-                            KeyCode::Left => self.list_items.unselect(),
-                            KeyCode::Down => self.list_items.next(),
-                            KeyCode::Up => self.list_items.previous(),
-                            KeyCode::Esc => {
-                                self.hide_popups();
-                            }
-                            _ => {}
-                        }
+    fn handle_events_log_list(&mut self, key: KeyEvent) -> io::Result<()> {
+        match key.code {
+            KeyCode::Char('f') => self.follow_mode = !self.follow_mode,
+            KeyCode::Char('c') => {
+                self.list_items.clear_all();
+                self.log_textarea = None;
+            }
+            KeyCode::Char('x') => self.list_items.set_cutoff(0),
+            KeyCode::Char('?') => self.app_mode = AppMode::ShowingKeybindings,
+            KeyCode::Char('/') => self.app_mode = AppMode::EditingFilter,
+            KeyCode::Tab => {
+                if self.list_items.selected_item().is_some() {
+                    self.app_mode = AppMode::FocusLogText;
+                }
+            }
 
-                        if let Some(log_text) = self.list_items.selected_item() {
-                            let ss: String = log_text.text().to_owned();
-                            let lines: Vec<_> = ss.lines().map(String::from).collect();
-                            self.log_textarea = Some(TextArea::new(lines));
-                            trace!("Log text some");
-                        } else {
-                            trace!("Log text none");
-                            self.log_textarea = None;
-                        }
-                    }
-                    AppMode::FocusLogText if key.kind == KeyEventKind::Press => match key.code {
-                        KeyCode::Esc => self.app_mode = AppMode::Normal,
-                        _ => {
-                            // trace!("Got input");
-                            self.log_textarea
-                                .as_mut()
-                                .expect("Log text area should never be None when in FocusLogMode")
-                                .input(key);
-                        }
-                    },
-                    AppMode::FocusLogText => {}
-                    AppMode::EditingFilter if key.kind == KeyEventKind::Press => match key.code {
-                        // KeyCode::Enter => {
-                        //     self.filter = Some(self.textarea.lines().iter().cloned().collect());
-                        //     trace!("Filter: {:?}", self.filter);
-                        //
-                        //     self.app_mode = AppMode::Normal;
-                        //     self.hide_popups()
-                        // }
-                        KeyCode::Esc => {
-                            trace!("Input: {:?}", self.filter);
+            KeyCode::PageUp => self
+                .list_items
+                .jump_relative(-((self.size.height - 4) as isize)),
+            KeyCode::PageDown => self
+                .list_items
+                .jump_relative((self.size.height - 4) as isize),
 
-                            self.app_mode = AppMode::Normal;
-                            self.hide_popups();
-                        }
-                        _ => {
-                            self.textarea.input(key);
-                        }
-                    },
-                    AppMode::EditingFilter => {} // Key up. We handle all events above
-                    AppMode::ShowingKeybindings => {
-                        match key.code {
-                            KeyCode::Up => {
-                                if let Some(n) = self.keybindings_state.selected_mut() {
-                                    if *n > 0 {
-                                        *n = (*n - 1).clamp(0, self.keybindings.len() - 1);
-                                    } else {
-                                        *n = 0;
-                                    }
-                                } else {
-                                    self.keybindings_state.select(Some(0));
-                                }
-                                // trace!("Keybind up {:?}", self.keybindings_state);
-                            }
-                            KeyCode::Down => {
-                                if let Some(n) = self.keybindings_state.selected_mut() {
-                                    *n = (*n + 1).clamp(0, self.keybindings.len() - 1);
-                                } else {
-                                    self.keybindings_state.select(Some(0));
-                                }
-                                // trace!("Keybind down {:?}", self.keybindings_state);
-                            }
-                            KeyCode::Esc => {
-                                self.app_mode = AppMode::Normal;
-                                self.hide_popups();
-                            }
-                            _ => {}
-                        }
+            KeyCode::Home => self.list_items.goto_start(),
+            KeyCode::End => self.list_items.goto_end(),
+            KeyCode::Left => self.list_items.unselect(),
+            KeyCode::Down => self.list_items.next(),
+            KeyCode::Up => self.list_items.previous(),
+            KeyCode::Esc => {
+                self.hide_popups();
+            }
+            _ => {}
+        }
+
+        if let Some(log_text) = self.list_items.selected_item() {
+            let ss: String = log_text.text().to_owned();
+            let lines: Vec<_> = ss.lines().map(String::from).collect();
+            self.log_textarea = Some(TextArea::new(lines));
+            trace!("Log text some");
+        } else {
+            trace!("Log text none");
+            self.log_textarea = None;
+        }
+
+        Ok(())
+    }
+
+    fn handle_events_log_text(&mut self, key: KeyEvent) -> io::Result<()> {
+        if key.kind == KeyEventKind::Press {
+            match key.code {
+                KeyCode::Esc => self.app_mode = AppMode::Normal,
+                _ => {
+                    if let Some(text_area) = &mut self.log_textarea {
+                        text_area.input(key);
                     }
                 }
             }
+        }
+
+        Ok(())
+    }
+
+    fn handle_events_filter(&mut self, key: KeyEvent) -> io::Result<()> {
+        if key.kind == KeyEventKind::Press {
+            match key.code {
+                // KeyCode::Enter => {
+                //     self.filter = Some(self.textarea.lines().iter().cloned().collect());
+                //     trace!("Filter: {:?}", self.filter);
+                //
+                //     self.app_mode = AppMode::Normal;
+                //     self.hide_popups()
+                // }
+                KeyCode::Esc => {
+                    trace!("Input: {:?}", self.filter);
+
+                    self.app_mode = AppMode::Normal;
+                    self.hide_popups();
+                }
+                _ => {
+                    self.textarea.input(key);
+                }
+            }
+        }
+
+        Ok(())
+    }
+
+    fn handle_events_show_keybindings(&mut self, key: KeyEvent) -> io::Result<()> {
+        match key.code {
+            KeyCode::Up => {
+                if let Some(n) = self.keybindings_state.selected_mut() {
+                    if *n > 0 {
+                        *n = (*n - 1).clamp(0, self.keybindings.len() - 1);
+                    } else {
+                        *n = 0;
+                    }
+                } else {
+                    self.keybindings_state.select(Some(0));
+                }
+                // trace!("Keybind up {:?}", self.keybindings_state);
+            }
+            KeyCode::Down => {
+                if let Some(n) = self.keybindings_state.selected_mut() {
+                    *n = (*n + 1).clamp(0, self.keybindings.len() - 1);
+                } else {
+                    self.keybindings_state.select(Some(0));
+                }
+                // trace!("Keybind down {:?}", self.keybindings_state);
+            }
+            KeyCode::Esc => {
+                self.app_mode = AppMode::Normal;
+                self.hide_popups();
+            }
+            _ => {}
         }
 
         Ok(())
@@ -333,8 +357,14 @@ impl<'a> App<'a> {
     }
 
     fn render_full_log(&mut self, f: &mut Frame, area: &Rect) {
+        let title = if self.app_mode == AppMode::FocusLogText {
+            "| >Content< |"
+        } else {
+            "|  Content  |"
+        };
+
         let block = Block::default()
-            .title("Content")
+            .title(title)
             .borders(Borders::ALL)
             .style(Style::default().bg(Color::Blue));
 
@@ -365,15 +395,27 @@ impl<'a> App<'a> {
             })
             .collect();
 
+        let highlight_symbol = if self.app_mode == AppMode::Normal {
+            ">> "
+        } else {
+            "   "
+        };
+
+        let title = if self.app_mode == AppMode::Normal {
+            "| >List< |"
+        } else {
+            "|  List  |"
+        };
+
         // Create a List from all list items and highlight the currently selected one
         let list_widget = List::new(items)
-            .block(Block::default().borders(Borders::ALL).title("List"))
+            .block(Block::default().borders(Borders::ALL).title(title))
             .highlight_style(
                 Style::default()
                     .bg(Color::LightGreen)
                     .add_modifier(Modifier::BOLD),
             )
-            .highlight_symbol(">> ");
+            .highlight_symbol(highlight_symbol);
 
         // Follow the list here?
 
